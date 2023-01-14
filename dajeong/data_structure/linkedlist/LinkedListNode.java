72fc14ef1d040fb53507d14bc798469d30f52147
DJ-archive <ydajeong7@gmail.com>
2023-01-14 16:30:40 +0900

    feat: 단방향 링크드리스트 구현 (삽입, 삭제, 조회)
    - 이전 파일에 수정할 부분이 있어 수정

---
+++ dajeong/data_structure/linkedlist/LinkedListNode.java
@@ -0,0 +1,70 @@
+package linkedlist;
+
+/*
+ * 단방향 링크드리스트 (Singly LinkedList) 간단히 java로 구현해보기
+ */
+
+class LinkedList {
+
+	Node header;
+
+	LinkedList() {
+		header = new Node();
+	}
+
+	void append(int d) {
+		Node end = new Node();
+		end.data = d;
+		Node n = header;
+		while (n.next != null) {
+			n = n.next;
+		}
+		n.next = end;
+	}
+
+	void delete(int d) {
+		Node n = header;
+		while (n.next != null) {
+			if (n.next.data == d) {
+				n.next = n.next.next;
+			} else {
+				n = n.next;
+			}
+		}
+	}
+
+	void retrieve() {
+		Node n = header.next;
+		while (n.next != null) {
+			System.out.print(n.data + " -> ");
+			n = n.next;
+		}
+		System.out.println(n.data);
+	}
+
+	static class Node {
+
+		int data;
+		Node next = null;
+	}
+
+}
+
+public class LinkedListNode {
+
+	public static void main(String[] args) {
+		LinkedList ll = new LinkedList();
+		ll.append(1);
+		ll.append(2);
+		ll.append(3);
+		ll.append(4);
+		ll.retrieve();
+		ll.delete(2);
+		ll.retrieve();
+	}
+}
+
+/* 결과
+ * 1 -> 2 -> 3 -> 4
+ * 1 -> 3 -> 4
+ */
