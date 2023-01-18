526e3bd6aafbe4e1174cd9c0780806b08b87d3a3
DJ-archive <ydajeong7@gmail.com>
2023-01-18 17:52:59 +0900

    0118 - stack 구현

---
+++ dajeong/data_structure/stack/Stack.java
@@ -0,0 +1,66 @@
+package stack;
+
+import java.util.EmptyStackException;
+
+/*
+ * Stack 구현하기
+ * LIFO
+ */
+public class Stack<T> {
+    class Node<T> {
+        private T data;
+        private Node<T> next;
+
+        public Node(T data) {
+            this.data = data;
+        }
+    }
+
+    private Node<T> top;
+    public T pop() {
+        if (top == null) {
+            throw new EmptyStackException();
+        }
+        T item = top.data;
+        top = top.next;
+        return item;
+    }
+
+    public void push(T item) {
+        Node<T> t= new Node<T>(item);
+        t.next = top;
+        top = t;
+    }
+
+    public T peek() {
+        if (top == null) {
+            throw new EmptyStackException();
+        }
+
+        return top.data;
+    }
+
+    public boolean isEmpty() {
+        return top == null;
+    }
+
+
+}
+
+class Test {
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<Integer>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+        System.out.println(s.pop());
+        System.out.println(s.pop());
+        System.out.println(s.peek());
+        System.out.println(s.pop());
+        System.out.println(s.isEmpty());
+        System.out.println(s.pop());
+        System.out.println(s.isEmpty());
+
+    }
+}
