1ed4af1b08d431f66c8be704528fe38434c26d26
DJ-archive <ydajeong7@gmail.com>
2023-02-07 18:01:12 +0900

    0207 - dp

---
+++ dajeong/algorithm/src/dp/Main_14501.java
@@ -0,0 +1,23 @@
+package dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_14501 {
+    /*
+     * d[i] = i번째 날까지 받을 수 있는 최대 금액 (확정된 금액)
+     *
+     */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+        }
+
+
+    }
+}
