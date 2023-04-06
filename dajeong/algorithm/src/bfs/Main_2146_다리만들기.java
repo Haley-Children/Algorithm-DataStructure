ce9ff3488f375266505c402de4e80074de24c797
DJ-archive <ydajeong7@gmail.com>
2023-04-06 23:40:44 +0900

    [기타] BOJ 2146 다리만들기 import, inner class 수정

--- dajeong/algorithm/src/bfs/Main_2146_다리만들기.java
+++ dajeong/algorithm/src/bfs/Main_2146_다리만들기.java
@@ -4,18 +4,18 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
-class Bridge {
-    int row,col;
 
-    public Bridge(int row, int col) {
-        this.row = row;
-        this.col = col;
-    }
-}
 public class Main_2146_다리만들기 {
+    class Bridge {
+        int row,col;
+
+        public Bridge(int row, int col) {
+            this.row = row;
+            this.col = col;
+        }
+    }
 
     static int[][] map; // 원본 데이터
     static int[][] dis; // 거리 배열
@@ -25,7 +25,8 @@ public class Main_2146_다리만들기 {
     static int[] diy = {0, 1, 0, -1};
     static int N;
     static ArrayDeque<Bridge> queue = new ArrayDeque<>(); // 다리를 놓는 bfs를 수행할 큐
-    public static void main(String[] args) throws IOException {
+
+    public void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         map = new int[N][N];
@@ -45,23 +46,25 @@ public class Main_2146_다리만들기 {
             for (int col = 0; col < N; col++) {
                 if (map[row][col] == 1 && !vis[row][col]) {
                     cnt++;
-                    bfs(row,col, cnt);
+                    bfs(row, col, cnt);
                 }
             }
         }
 
         vis = new boolean[N][N]; // 방문 배열 갱신
         int ans = Integer.MAX_VALUE; // 정답 (최소 다리 갯수)
-        while(!queue.isEmpty()) {
+        while (!queue.isEmpty()) {
             Bridge cur = queue.poll();
             for (int i = 0; i < 4; i++) {
                 int nx = cur.row + dix[i];
                 int ny = cur.col + diy[i];
-                if (nx <0 || nx >= N || ny < 0 || ny >= N) continue;
+                if (nx < 0 || nx >= N || ny < 0 || ny >= N) {
+                    continue;
+                }
                 if (!vis[nx][ny] && landNumMap[nx][ny] == 0) { // 바다를 만날 경우
                     dis[nx][ny] = dis[cur.row][cur.col] + 1; // 최단 거리 기록
                     landNumMap[nx][ny] = landNumMap[cur.row][cur.col]; // 어느 섬에서 온 다리인지 표시
-                    queue.offer(new Bridge(nx,ny)); // Bridge 객체 저장
+                    queue.offer(new Bridge(nx, ny)); // Bridge 객체 저장
                     vis[nx][ny] = true;
                 } else if (dis[nx][ny] > 0 || landNumMap[nx][ny] >= 1) { // 이미 방문한 바다 혹은 섬에 다다를경우
                     if (landNumMap[nx][ny] != landNumMap[cur.row][cur.col]) { // 다른 섬일 때
@@ -76,22 +79,24 @@ public class Main_2146_다리만들기 {
 
     // landNumMap 이용하기
     // 바다가 아니라 바다에 인접한 섬을 큐에 넣기
-    private static void bfs(int row, int col, int cnt) {
+    private void bfs(int row, int col, int cnt) {
         ArrayDeque<Bridge> checkNumQueue = new ArrayDeque<>();
         checkNumQueue.offer(new Bridge(row, col));
         vis[row][col] = true;
         landNumMap[row][col] = cnt;
 
-        while(!checkNumQueue.isEmpty()) {
+        while (!checkNumQueue.isEmpty()) {
             Bridge cur = checkNumQueue.poll();
             for (int i = 0; i < 4; i++) {
                 int nx = cur.row + dix[i];
                 int ny = cur.col + diy[i];
-                if (nx <0 || nx >= N || ny < 0 || ny >= N || vis[nx][ny]) continue;
+                if (nx < 0 || nx >= N || ny < 0 || ny >= N || vis[nx][ny]) {
+                    continue;
+                }
                 if (map[nx][ny] == 0) { // 섬에 인접한 바다일 경우
                     queue.offer(new Bridge(cur.row, cur.col)); // 섬을 넣어주라고 위에 적었으면서 바다 넣는건 뭐임???!!!
                 } else { // 섬일 경우
-                    checkNumQueue.offer(new Bridge(nx,ny));
+                    checkNumQueue.offer(new Bridge(nx, ny));
                     vis[nx][ny] = true;
                     landNumMap[nx][ny] = cnt;
                 }
@@ -101,3 +106,4 @@ public class Main_2146_다리만들기 {
     }
 
 }
+
