cc39387e04742f2799ebd42d9f252f8f88792908
DJ-archive <ydajeong7@gmail.com>
2023-05-02 06:49:09 +0900

    [알고리즘] BOJ 17219 비밀번호찾기

---
+++ dajeong/algorithm/src/hash/Main_17219_비밀번호찾기.java
@@ -0,0 +1,28 @@
+package hash;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main_17219_비밀번호찾기 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        HashMap<String, String> map = new HashMap<>();
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            String site = st.nextToken();
+            String pw = st.nextToken();
+            map.put(site,pw);
+        }
+        for (int i = 0; i < M; i++) {
+            System.out.println(map.get(br.readLine()));
+        }
+    }
+
+}
